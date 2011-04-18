171c1492eb196e013e265e1aeace292e688d6616
Marcel Caraciolo <caraciol@gmail.com>
2011-04-18 13:59:22 -0300

    Added DifferStorage for SlopeOneRecommender.

---
+++ crab/recommender/utils.py
@@ -0,0 +1,108 @@
+#-*- coding:utf-8 -*-
+
+
+#========================================================================
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#========================================================================
+
+
+
+"""
+:mod:`utils` -- the utils recommendation modules  
+================================================================
+
+This module contains functions and classes to help the recommendation process.
+
+"""
+
+class DiffStorage(object):
+	'''
+	An implementation of DiffStorage that merely stores item-item diffs in memory. 
+	Caution: It may consume a great deal of memory due to larger datasets.
+	'''
+	def __init__(self,model,stdDevWeighted):
+		''' DiffStorage Class Constructor
+		     `model` is the data source model
+			 `stdDevWeighted` is a flag that if it is True, use standard deviation weighting of diffs
+		'''
+		self.model = model
+		self.stdDevWeighted = stdDevWeighted
+		self._diffStorage = {}
+		self._freqs = {}
+		self._recommendableItems = []
+		self._buildAverageDiffs()
+	
+	def _buildAverageDiffs(self):
+		self._diffStorage = {}
+		for userID in self.model.UserIDs():
+			self.processOneUser(userID)
+		self.pruneDiffs()
+		self.updateAllRecommendableItems()
+		
+	def recommendableItems(self):
+		return self._recommendableItems
+	
+	def updateAllRecommendableItems(self):
+		self._recommendableItems = []
+		for itemID in self._diffStorage:
+			self._recommendableItems.append(itemID)
+		self._recommendableItems.sort()
+		
+	def diffsAverage(self,userID,itemID,prefs):
+		return [ self.diff(itemID,itemID2)   for  itemID2,rating in prefs]
+	
+	def diff(self,itemIDA,itemIDB):
+		if itemIDA in self._diffStorage:
+			if itemIDB in self._diffStorage[itemIDA]:
+				return self._diffStorage[itemIDA][itemIDB]
+			elif itemIDB in self._diffStorage:
+				if itemIDA in self._diffStorage[itemIDB]:
+					return self._diffStorage[itemIDB][itemIDA]
+					
+	def count(self,itemID,itemID2):
+		try:
+			return self.freqs[itemID][itemIDB]
+		except KeyError:
+			return self.freqs[itemIDB][itemID]			
+	
+	def pruneDiffs(self):
+		'''
+		Go back and prune irrelevant diffs. Irrelevant means here, represented by one data point, so possibly unreliable
+		'''
+		for item1 in self._freqs.keys():
+			for item2 in self._freqs[item1].keys():
+				if self._freqs[item1][item2] <=1:
+					del self._freqs[item1][item2]
+					del self._diffStorage[item1][item2]
+					if len(self._diffStorage[item1])==0:
+						break
+						
+											
+	def processOneUser(self,userID):
+		userPreferences = self.model.PreferencesFromUser(userID)
+		for indexA,preferenceA in enumerate(userPreferences):
+			itemID1,rating1 = preferenceA
+			self._diffStorage.setdefault(itemID1,{})
+			self._freqs.setdefault(itemID1,{})
+			for indexB,preferenceB in enumerate(userPreferences[indexA+1:]):
+				itemID2,rating2 = preferenceB
+				if self.stdDevWeighted:
+					pass
+				else:
+					self._diffStorage[itemID1].setdefault(itemID2,0.0)
+					self._freqs[itemID1].setdefault(itemID2,0)
+					self._diffStorage[itemID1][itemID2]+= (rating1 - rating2)
+					self._freqs[itemID1][itemID2]+=1
--- crab/tests/test_recommender.py
+++ crab/tests/test_recommender.py
@@ -24,12 +24,15 @@
 __author__ = 'marcel@orygens.com'
 
 import unittest
+import sys
 
+#sys.path.append('/Users/marcelcaraciolo/Desktop/crab/crab/crab')
 
 
 from models.datamodel import *
 from recommender.topmatches import *
 from recommender.recommender import UserRecommender, ItemRecommender
+from recommender.utils import DiffStorage
 from similarities.similarity import UserSimilarity, ItemSimilarity
 from similarities.similarity_distance import *
 from scoring.scorer import TanHScorer, NaiveScorer
@@ -157,6 +160,92 @@ class TestUserBasedRecommender(unittest.TestCase):
 		recSys = UserRecommender(self.model,self.similarity,self.neighbor,False)
 		self.assertEquals(['Just My Luck'],recSys.recommend(userID,1))
 		
+		
+		
+class TestSlopeOneRecommender(unittest.TestCase):
+	
+	def setUp(self):
+		#SIMILARITY BY RATES.
+		movies={'Marcel Caraciolo': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+		 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+		 'The Night Listener': 3.0},
+		'Luciana Nunes': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
+		 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
+		 'You, Me and Dupree': 3.5}, 
+		'Leopoldo Pires': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+		 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+		'Lorena Abreu': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+		 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+		 'You, Me and Dupree': 2.5},
+		'Steve Gates': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+		 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+		 'You, Me and Dupree': 2.0}, 
+		'Sheldom': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+		 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+		'Penny Frewman': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0},
+		'Maria Gabriela': {}}
+
+		self.model = DictDataModel(movies)	
+		
+	def test_create_diffStorage(self):
+		storage = DiffStorage(self.model,False)
+		self.assertEquals(storage.stdDevWeighted,False)
+		self.assertEquals(storage.model,self.model)
+	
+	def test_buildAveragesDiff(self):
+		storage = DiffStorage(self.model,False)
+		self.assertEquals(storage._diffStorage,{'Lady in the Water': {'Snakes on a Plane': -4.0, 'You, Me and Dupree': 0.0, 'Superman Returns': -6.0, 'The Night Listener': -2.0}, 
+		                      'Snakes on a Plane': {'You, Me and Dupree': 8.0, 'Superman Returns': -2.0, 'The Night Listener': 1.0}, 
+		                      'Just My Luck': {'Snakes on a Plane': -5.0, 'Lady in the Water': -2.0, 'You, Me and Dupree': -1.0, 'Superman Returns': -6.0, 'The Night Listener': -4.0}, 
+		                      'Superman Returns': {'You, Me and Dupree': 9.5, 'The Night Listener': 3.5}, 
+		                      'You, Me and Dupree': {}, 
+		                      'The Night Listener': {'You, Me and Dupree': 2.5}})
+		
+		self.assertEquals(storage._freqs,{'Lady in the Water': {'Snakes on a Plane': 5, 'You, Me and Dupree': 4, 'Superman Returns': 5, 'The Night Listener': 5},
+		             'Snakes on a Plane': {'You, Me and Dupree': 6, 'Superman Returns': 7, 'The Night Listener': 6}, 
+		             'Just My Luck': {'Snakes on a Plane': 4, 'Lady in the Water': 3, 'You, Me and Dupree': 4, 'Superman Returns': 4, 'The Night Listener': 4},
+		             'Superman Returns': {'You, Me and Dupree': 6, 'The Night Listener': 6}, 
+		             'You, Me and Dupree': {}, 'The Night Listener': {'You, Me and Dupree': 5}} )
+		
+		self.assertEquals(storage._recommendableItems,['Just My Luck', 'Lady in the Water', 'Snakes on a Plane', 'Superman Returns', 'The Night Listener', 'You, Me and Dupree'])
+		
+		
+		storage = DiffStorage(DictDataModel({}),False)
+		self.assertEquals(storage._diffStorage,{})
+		self.assertEquals(storage._freqs,{})
+		self.assertEquals(storage._recommendableItems,[])
+				
+		storage = DiffStorage(DictDataModel({}),False)
+		self.assertEquals(storage._diffStorage,{})
+		self.assertEquals(storage._freqs,{})
+		self.assertEquals(storage._recommendableItems,[])
+		
+		storage = DiffStorage(DictDataModel({'Maria':{'A':4.0}}),False)
+		self.assertEquals(storage._diffStorage,{'A': {}})
+		self.assertEquals(storage._freqs,{'A': {} })
+		self.assertEquals(storage._recommendableItems,['A'])
+
+		storage = DiffStorage(DictDataModel({'Maria':{'A':4.0}, 'Joao':{'B': 5.0} }),False)
+		self.assertEquals(storage._diffStorage,{'A': {}, 'B':{}})
+		self.assertEquals(storage._freqs,{'A': {}, 'B': {} })
+		self.assertEquals(storage._recommendableItems,['A','B'])
+			
+		storage = DiffStorage(DictDataModel({'Maria':{'A':4.0, 'B': 2.0}, 'Joao':{'B': 5.0} }),False)
+		self.assertEquals(storage._diffStorage,{'A': {}, 'B':{}})
+		self.assertEquals(storage._freqs,{'A': {}, 'B': {} })
+		self.assertEquals(storage._recommendableItems,['A','B'])
+		
+		storage = DiffStorage(DictDataModel({'Maria':{'A':4.0, 'B': 2.0}, 'Joao':{'B': 5.0, 'A':5.0}, 'Flavia':{'A': 2.0, 'C': 3.0} }),False)
+		self.assertEquals(storage._diffStorage,{'A': {'B': 2.0}, 'B':{}, 'C':{}})
+		self.assertEquals(storage._freqs,{'A': {'B':2}, 'B': {}, 'C': {} })
+		self.assertEquals(storage._recommendableItems,['A','B', 'C'])
+	
+
+	
+	
+		
+		
+				
 
 class TestItemBasedRecommender(unittest.TestCase):
 		
@@ -279,6 +368,7 @@ def suite():
 	suite = unittest.TestSuite()
 	suite.addTests(unittest.makeSuite(TestUserBasedRecommender))
 	suite.addTests(unittest.makeSuite(TestItemBasedRecommender))
+	suite.addTests(unittest.makeSuite(TestSlopeOneRecommender))
 	
 
 	return suite
