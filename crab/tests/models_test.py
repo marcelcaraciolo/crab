c980face4ffd1dfbfd0272a4f6c9b7f8f78c4832
Wichert Akkerman <wichert@wiggy.net>
2011-08-29 23:25:17 +0200

    More PEP8

--- crab/tests/models_test.py
+++ crab/tests/models_test.py
@@ -30,136 +30,206 @@ from models.datamodel import *
 
 
 class TestDictModel(unittest.TestCase):
-    
-    def setUp(self):        
+
+    def setUp(self):
         # SIMILARITY BY RATES.
-        self.movies={'Marcel Caraciolo': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
-         'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
-         'The Night Listener': 3.0},
-        'Luciana Nunes': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
-         'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
-         'You, Me and Dupree': 3.5}, 
-        'Leopoldo Pires': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
-         'Superman Returns': 3.5, 'The Night Listener': 4.0},
-        'Lorena Abreu': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
-         'The Night Listener': 4.5, 'Superman Returns': 4.0, 
-         'You, Me and Dupree': 2.5},
-        'Steve Gates': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
-         'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
-         'You, Me and Dupree': 2.0}, 
-        'Sheldom': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-         'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-        'Penny Frewman': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0},
-        'Maria Gabriela': {}}
-        
-    def test_create_DictModel(self):        
+        self.movies = {
+                'Marcel Caraciolo': {
+                    'Lady in the Water': 2.5,
+                    'Snakes on a Plane': 3.5,
+                    'Just My Luck': 3.0,
+                    'Superman Returns': 3.5,
+                    'You, Me and Dupree': 2.5,
+                    'The Night Listener': 3.0},
+                'Luciana Nunes': {
+                    'Lady in the Water': 3.0,
+                    'Snakes on a Plane': 3.5,
+                    'Just My Luck': 1.5,
+                    'Superman Returns': 5.0,
+                    'The Night Listener': 3.0,
+                    'You, Me and Dupree': 3.5},
+                'Leopoldo Pires': {
+                    'Lady in the Water': 2.5,
+                    'Snakes on a Plane': 3.0,
+                    'Superman Returns': 3.5,
+                    'The Night Listener': 4.0},
+                'Lorena Abreu': {
+                    'Snakes on a Plane': 3.5,
+                    'Just My Luck': 3.0,
+                    'The Night Listener': 4.5,
+                    'Superman Returns': 4.0,
+                    'You, Me and Dupree': 2.5},
+                'Steve Gates': {
+                    'Lady in the Water': 3.0,
+                    'Snakes on a Plane': 4.0,
+                    'Just My Luck': 2.0,
+                    'Superman Returns': 3.0,
+                    'The Night Listener': 3.0,
+                    'You, Me and Dupree': 2.0},
+                'Sheldom': {
+                    'Lady in the Water': 3.0,
+                    'Snakes on a Plane': 4.0,
+                    'The Night Listener': 3.0,
+                    'Superman Returns': 5.0,
+                    'You, Me and Dupree': 3.5},
+                'Penny Frewman': {
+                    'Snakes on a Plane': 4.5,
+                    'You, Me and Dupree': 1.0,
+                    'Superman Returns': 4.0},
+                'Maria Gabriela': {}}
+
+    def test_create_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(self.movies,model.dataU)
-        
+        self.assertEquals(self.movies, model.dataU)
+
     def test_UserIDs_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(['Leopoldo Pires', 'Lorena Abreu', 'Luciana Nunes', 'Marcel Caraciolo',
-             'Maria Gabriela', 'Penny Frewman', 'Sheldom', 'Steve Gates'] ,model.UserIDs())
-    
+        self.assertEquals(
+                ['Leopoldo Pires', 'Lorena Abreu', 'Luciana Nunes',
+                 'Marcel Caraciolo', 'Maria Gabriela', 'Penny Frewman',
+                 'Sheldom', 'Steve Gates'],
+                model.UserIDs())
+
     def test_ItemIDs_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(['Just My Luck', 'Lady in the Water', 'Snakes on a Plane', 
-           'Superman Returns', 'The Night Listener', 'You, Me and Dupree'],model.ItemIDs())
-        
+        self.assertEquals(
+                ['Just My Luck', 'Lady in the Water', 'Snakes on a Plane',
+                 'Superman Returns', 'The Night Listener',
+                 'You, Me and Dupree'],
+                model.ItemIDs())
+
     def test_PreferencesFromUser_Existing_UserDictModel(self):
         model = DictDataModel(self.movies)
         # Ordered by ItemID
-        self.assertEquals([('Just My Luck', 3.0), ('Snakes on a Plane', 3.5), ('Superman Returns', 4.0), 
-        ('The Night Listener', 4.5), ('You, Me and Dupree', 2.5)],model.PreferencesFromUser('Lorena Abreu'))
+        self.assertEquals([('Just My Luck', 3.0),
+                           ('Snakes on a Plane', 3.5),
+                           ('Superman Returns', 4.0),
+                           ('The Night Listener', 4.5),
+                           ('You, Me and Dupree', 2.5)],
+                          model.PreferencesFromUser('Lorena Abreu'))
         # Ordered by Rate (Reverse)
-        self.assertEquals([('The Night Listener', 4.5), ('Superman Returns', 4.0), ('Snakes on a Plane', 3.5), 
-        ('Just My Luck', 3.0), ('You, Me and Dupree', 2.5)],model.PreferencesFromUser('Lorena Abreu',orderByID=False))
-        
+        self.assertEquals([('The Night Listener', 4.5),
+                           ('Superman Returns', 4.0),
+                           ('Snakes on a Plane', 3.5),
+                           ('Just My Luck', 3.0),
+                           ('You, Me and Dupree', 2.5)],
+                   model.PreferencesFromUser('Lorena Abreu', orderByID=False))
 
     def test_PreferencesFromUser_Existing_User_No_PreferencesDictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals([],model.PreferencesFromUser('Maria Gabriela'))
+        self.assertEquals([], model.PreferencesFromUser('Maria Gabriela'))
 
     def test_PreferencesFromUser_Non_Existing_User_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertRaises(ValueError, model.PreferencesFromUser, 'Flavia')    
-    
+        self.assertRaises(ValueError, model.PreferencesFromUser, 'Flavia')
+
     def test_ItemIDsFromUser_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(['Just My Luck', 'Lady in the Water', 'Snakes on a Plane', 
-            'Superman Returns', 'The Night Listener', 'You, Me and Dupree'] , model.ItemIDsFromUser('Marcel Caraciolo'))
-    
+        self.assertEquals(['Just My Luck', 'Lady in the Water',
+                           'Snakes on a Plane', 'Superman Returns',
+                           'The Night Listener', 'You, Me and Dupree'],
+                           model.ItemIDsFromUser('Marcel Caraciolo'))
+
     def test_PreferencesForItem_Existing_Item_DictModel(self):
         model = DictDataModel(self.movies)
         # Ordered by ItemID
-        self.assertEquals([('Leopoldo Pires', 3.5), ('Lorena Abreu', 4.0), ('Luciana Nunes', 5.0), ('Marcel Caraciolo', 3.5), 
-        ('Penny Frewman', 4.0), ('Sheldom', 5.0), ('Steve Gates', 3.0)],model.PreferencesForItem('Superman Returns'))
+        self.assertEquals([('Leopoldo Pires', 3.5),
+                           ('Lorena Abreu', 4.0),
+                           ('Luciana Nunes', 5.0),
+                           ('Marcel Caraciolo', 3.5),
+                           ('Penny Frewman', 4.0),
+                           ('Sheldom', 5.0),
+                           ('Steve Gates', 3.0)],
+                          model.PreferencesForItem('Superman Returns'))
         # Ordered by Rate (Reverse)
-        self.assertEquals([('Luciana Nunes', 5.0), ('Sheldom', 5.0), ('Penny Frewman', 4.0), ('Lorena Abreu', 4.0), 
-        ('Leopoldo Pires', 3.5), ('Marcel Caraciolo', 3.5), ('Steve Gates', 3.0)], model.PreferencesForItem('Superman Returns',orderByID=False))
+        self.assertEquals([('Luciana Nunes', 5.0),
+                           ('Sheldom', 5.0),
+                           ('Penny Frewman', 4.0),
+                           ('Lorena Abreu', 4.0),
+                           ('Leopoldo Pires', 3.5),
+                           ('Marcel Caraciolo', 3.5),
+                           ('Steve Gates', 3.0)],
+              model.PreferencesForItem('Superman Returns', orderByID=False))
 
     def test_PreferencesForItem_Existing_Item_No_PreferencesDictModel(self):
         model = DictDataModel(self.movies)
-        # BUG ? If there is an item without rating in the model, it must return [] or raise an Exception ?
-        self.assertRaises(ValueError, model.PreferencesFromUser, 'Night Listener')        
+        # BUG ? If there is an item without rating in the model, it must return
+        # [] or raise an Exception ?
+        self.assertRaises(ValueError,
+                model.PreferencesFromUser, 'Night Listener')
 
     def test_PreferencesForItem_Non_Existing_Item_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertRaises(ValueError, model.PreferencesFromUser, 'Back to the Future')        
-        
+        self.assertRaises(ValueError,
+                model.PreferencesFromUser, 'Back to the Future')
+
     def test_PreferenceValue_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(3.5 , model.PreferenceValue('Marcel Caraciolo','Superman Returns'))
-    
+        self.assertEquals(3.5,
+                model.PreferenceValue('Marcel Caraciolo', 'Superman Returns'))
+
     def test_NumUsers_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(8,model.NumUsers())
-        
+        self.assertEquals(8, model.NumUsers())
+
     def test_NumItems_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(6,model.NumItems())    
-        
+        self.assertEquals(6, model.NumItems())
+
     def test_NumUsersWithPreferenceFor_Invalid_User_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor)    
-        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor,'SuperMan Returns','Just My Luck', 'Lady in The Water')    
-        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor,'SuperMan Returns','Back to the future')    
-            
+        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor)
+        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor,
+                'SuperMan Returns', 'Just My Luck', 'Lady in The Water')
+        self.assertRaises(ValueError, model.NumUsersWithPreferenceFor,
+                'SuperMan Returns', 'Back to the future')
+
     def test_NumUsersWithPreferenceFor_One_User_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(7, model.NumUsersWithPreferenceFor('Superman Returns'))    
+        self.assertEquals(7,
+                model.NumUsersWithPreferenceFor('Superman Returns'))
 
     def test_NumUsersWithPreferenceFor_Two_Users_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals(4, model.NumUsersWithPreferenceFor('Superman Returns','Just My Luck'))
-        
+        self.assertEquals(4, model.NumUsersWithPreferenceFor(
+                    'Superman Returns', 'Just My Luck'))
+
     def test_hasPreferenceValues_DictModel(self):
         model = DictDataModel(self.movies)
         self.assertEquals(True, model.hasPreferenceValues())
-    
+
     def test_MaxPreference_DictModel(self):
         model = DictDataModel(self.movies)
         self.assertEquals(5.0, model.MaxPreference())
-    
+
     def test_MinPreference_DictModel(self):
         model = DictDataModel(self.movies)
         self.assertEquals(1.0, model.MinPreference())
-    
+
     def test_get_item_DictModel(self):
         model = DictDataModel(self.movies)
-        self.assertEquals([('Just My Luck', 3.0), ('Lady in the Water', 2.5), ('Snakes on a Plane', 3.5), 
-        ('Superman Returns', 3.5), ('The Night Listener', 3.0), ('You, Me and Dupree', 2.5)] , model['Marcel Caraciolo'])    
-        
+        self.assertEquals([('Just My Luck', 3.0),
+                           ('Lady in the Water', 2.5),
+                           ('Snakes on a Plane', 3.5),
+                           ('Superman Returns', 3.5),
+                           ('The Night Listener', 3.0),
+                           ('You, Me and Dupree', 2.5)],
+                          model['Marcel Caraciolo'])
+
     def test_iter_DictModel(self):
         model = DictDataModel(self.movies)
-        elements =  [ pref  for pref in model ]
-        self.assertEquals(('Leopoldo Pires', [('Lady in the Water', 2.5), ('Snakes on a Plane', 3.0), 
-        ('Superman Returns', 3.5), ('The Night Listener', 4.0)]), elements[0])
-            
+        elements = [pref  for pref in model]
+        self.assertEquals(('Leopoldo Pires', [('Lady in the Water', 2.5),
+                                              ('Snakes on a Plane', 3.0),
+                                              ('Superman Returns', 3.5),
+                                              ('The Night Listener', 4.0)]),
+                          elements[0])
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTests(unittest.makeSuite(TestDictModel))
-    
+
     return suite
 
 if __name__ == '__main__':
